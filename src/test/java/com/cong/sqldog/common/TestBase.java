043bba9ad0e48a13fcfa4f3e9b46fb3748460444
lhccong <liuhuaicong@zhut.cn>
2024-07-24 16:11:56 +0800

    feat:引入单元测试基本配置

---
+++ src/test/java/com/cong/sqldog/common/TestBase.java
@@ -0,0 +1,34 @@
+package com.cong.sqldog.common;
+
+import com.cong.sqldog.MainApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import redis.embedded.RedisServer;
+
+@SpringBootTest(
+        classes = { MainApplication.class, },
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        properties = {"spring.profiles.active=test"}
+)
+@Slf4j
+@AutoConfigureMockMvc
+public abstract class TestBase {
+
+    protected static final Long TEST_GROUP_ID = 1L;
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+
+    @BeforeAll
+    public static void beforeAll() {
+        RedisServer redisServer = RedisServer.builder().setting("maxheap 200m").port(6379).build();
+        redisServer.start();
+    }
+
+
+}
