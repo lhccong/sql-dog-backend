ae589c4162b89a96dd61de2d0a2ff647e061a634
lhccong <liuhuaicong@zhut.cn>
2024-07-24 15:41:26 +0800

    feat:新增关卡题目表

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -65,15 +65,32 @@ create index idx_name
     on field_info (name);
 
 -- SQL 执行记录表
-create table if not exists sql_record_info
+create table if not exists level_info
 (
-    id            bigint auto_increment comment 'id' primary key,
-    sqlContent          varchar(512)                       null comment '执行 SQL 的内容',
-    sqlAnalyzeResult     varchar(512)                       null comment 'SQL 分析结果',
-    reviewStatus  int      default 0                 not null comment '状态（0-待审核, 1-通过, 2-拒绝）',
-    reviewMessage varchar(512)                       null comment '审核信息',
-    userId        bigint                             not null comment '创建用户 id',
-    createTime    datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete      tinyint  default 0                 not null comment '是否删除'
+    id               bigint auto_increment comment 'id' primary key,
+    sqlContent       varchar(512)                       null comment '执行 SQL 的内容',
+    sqlAnalyzeResult varchar(512)                       null comment 'SQL 分析结果',
+    reviewStatus     int      default 0                 not null comment '状态（0-待审核, 1-通过, 2-拒绝）',
+    reviewMessage    varchar(512)                       null comment '审核信息',
+    userId           bigint                             not null comment '创建用户 id',
+    createTime       datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime       datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete         tinyint  default 0                 not null comment '是否删除'
 ) comment 'SQL执行记录';
+
+-- 关卡题目表
+create table if not exists topic_level
+(
+    id         bigint auto_increment comment 'id' primary key,
+    title      varchar(64)                        null comment '关卡中文名',
+    initSQL    varchar(512)                       null comment '初始化 SQL',
+    mdContent  text                               not null comment '关卡教程 Markdown 文档',
+    defaultSQL varchar(512)                       null comment '关卡初始化后默认执行的语句，一般是查询全表',
+    answer     varchar(512)                       null comment '关卡标准答案',
+    hint       varchar(256)                       null comment '关卡提示',
+    type       varchar(256)                       null comment '关卡类别，custom 自定义、system 系统',
+    userId     bigint                             not null comment '创建用户 id',
+    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete   tinyint  default 0                 not null comment '是否删除'
+) comment '关卡题目';
