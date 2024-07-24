62373f101c0a7e034e22e3f41b5fca90832ce596
lhccong <liuhuaicong@zhut.cn>
2024-07-24 15:00:59 +0800

    feat:新增 SQL 执行记录表

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -63,3 +63,17 @@ create index idx_fieldName
 
 create index idx_name
     on field_info (name);
+
+-- SQL 执行记录表
+create table if not exists sql_record_info
+(
+    id            bigint auto_increment comment 'id' primary key,
+    sqlContent          varchar(512)                       null comment '执行 SQL 的内容',
+    sqlAnalyzeResult     varchar(512)                       null comment 'SQL 分析结果',
+    reviewStatus  int      default 0                 not null comment '状态（0-待审核, 1-通过, 2-拒绝）',
+    reviewMessage varchar(512)                       null comment '审核信息',
+    userId        bigint                             not null comment '创建用户 id',
+    createTime    datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete      tinyint  default 0                 not null comment '是否删除'
+) comment 'SQL执行记录';
