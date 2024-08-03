c529382924aef7c6a58e05eebd4e594f37e04659
lhccong <771901874@qq.com>
2024-08-03 13:45:54 +0800

    fix：移除使用 embedded-redis 依赖因为 mac 芯片不兼容

--- src/test/java/com/cong/sqldog/common/TestBase.java
+++ src/test/java/com/cong/sqldog/common/TestBase.java
@@ -2,12 +2,10 @@ package com.cong.sqldog.common;
 
 import com.cong.sqldog.MainApplication;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import redis.embedded.RedisServer;
 
 @SpringBootTest(
         classes = { MainApplication.class, },
