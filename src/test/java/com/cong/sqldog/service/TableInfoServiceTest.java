60b8a5d1caf997beb72ac85128c7a2f7763ad836
LBC <541014133@qq.com>
2024-08-06 18:02:13 +0800

    fix: 补充 table_info 表测试

--- src/test/java/com/cong/sqldog/service/TableInfoServiceTest.java
+++ src/test/java/com/cong/sqldog/service/TableInfoServiceTest.java
@@ -1,54 +1,54 @@
 package com.cong.sqldog.service;
 
-import cn.dev33.satoken.stp.StpUtil;
+import cn.dev33.satoken.stp.StpLogic;
 import cn.hutool.json.JSONUtil;
 import com.cong.sqldog.common.DeleteRequest;
+import com.cong.sqldog.common.ReviewRequest;
 import com.cong.sqldog.common.TestBase;
 import com.cong.sqldog.model.dto.tableinfo.TableInfoAddRequest;
 import com.cong.sqldog.model.dto.tableinfo.TableInfoEditRequest;
-import com.cong.sqldog.model.vo.LoginUserVO;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.annotation.Resource;
+import com.cong.sqldog.model.dto.tableinfo.TableInfoUpdateRequest;
+import com.cong.sqldog.model.entity.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockCookie;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.mockito.ArgumentMatchers.any;
+
 /**
  * TableInfo 单元测试类
+ *
  * @author shing
  */
 class TableInfoServiceTest extends TestBase {
 
-    private static final Logger log = LoggerFactory.getLogger(TableInfoServiceTest.class);
+    @MockBean
+    private UserService userService;
 
-    @Resource
-    UserService userService;
+    @MockBean
+    StpLogic stpLogic;
 
     /**
-     * 获取token
+     * 模拟已登录用户,
      */
-    private String getToken() {
-        // 模拟用户登录
-        String userAccount = "cong";
-        String userPassword = "12345678";
-        String token = StpUtil.getTokenValue();
-        if (token == null) {
-            try {
-                LoginUserVO loginUserVO = userService.userLogin(userAccount, userPassword);
-                // 记录用户的登录态
-                StpUtil.login(loginUserVO.getId());
-                // 获取token
-                token = StpUtil.getTokenValue();
-            } catch (Exception e) {
-                // 登录失败，打印错误信息
-                log.error("登录失败：{}", e.getMessage());
-            }
-        }
-        return token;
+    @BeforeEach
+    void setUp() {
+        User mockUser = new User();
+        mockUser.setId(1816001696590692353L);
+        mockUser.setUserRole("admin");
+
+        // 模拟 getLoginUser 方法返回 mockUser
+        Mockito.when(userService.getLoginUser()).thenReturn(mockUser);
+        Mockito.when(userService.isAdmin()).thenReturn(true);
+
+        //模拟 userService.getById 方法返回mockUser
+        Mockito.when(userService.getById(mockUser.getId())).thenReturn(mockUser);
+        Mockito.when(stpLogic.getLoginId(any())).thenAnswer(invocation -> mockUser.getId().toString());
+        Mockito.when(stpLogic.getLoginId()).thenAnswer(invocation -> mockUser.getId().toString());
     }
 
     /**
@@ -56,23 +56,14 @@ class TableInfoServiceTest extends TestBase {
      */
     @Test
     void addTable() throws Exception {
-        // 获取token
-        String token = getToken();
-        log.info("token: {}", token);
         // 准备测试数据
         TableInfoAddRequest tableInfoAddRequest = new TableInfoAddRequest();
         String content = "{\"id\":\"12345\",\"name\":\"shing\",\"age\":\"18\"}"; // 修改为对象格式
         tableInfoAddRequest.setContent(JSONUtil.toJsonStr(content));
-        String name = "test3";
-        tableInfoAddRequest.setName(name);
-
-        // 创建cookie并添加到请求中
-        MockCookie cookie = new MockCookie(StpUtil.getTokenName(), token);
-
+        tableInfoAddRequest.setName("test3");
 
         // 发送请求并验证结果
         mockMvc.perform(MockMvcRequestBuilders.post("/tableInfo/add")
-                        .cookie(cookie) // 将token添加到cookie中
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(JSONUtil.toJsonStr(tableInfoAddRequest))) // 将整个对象序列化为JSON字符串并作为请求体发送
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -85,55 +76,53 @@ class TableInfoServiceTest extends TestBase {
      */
     @Test
     void deleteTable() throws Exception {
-        // 获取token
-        String token = getToken();
-
         // 准备测试数据
         DeleteRequest deleteRequest = new DeleteRequest();
-        long id = 1819403133009338369L;
-        deleteRequest.setId(id);
-
-        // 创建cookie并添加到请求中
-        MockCookie cookie = new MockCookie(StpUtil.getTokenName(), token);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        String requestBody = objectMapper.writeValueAsString(deleteRequest);
+        deleteRequest.setId(1820740567387107329L);
 
         // 发送请求并验证结果
         mockMvc.perform(MockMvcRequestBuilders.post("/tableInfo/delete")
-                        .content(requestBody) // 将请求体添加到请求中
-                        .contentType(MediaType.APPLICATION_JSON) // 设置请求体的内容类型为JSON
-                        .cookie(cookie)) // 将token添加到cookie中
+                        .content(JSONUtil.toJsonStr(deleteRequest)) // 将整个对象序列化为JSON字符串并作为请求体发送
+                        .contentType(MediaType.APPLICATION_JSON)) // 设置请求体的内容类型为JSON
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(0))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("ok"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data").value(true));
-
     }
 
     /**
      * 编辑表格信息测试
-
      */
     @Test
     void editTable() throws Exception {
-        // 获取token
-        String token = getToken();
         // 准备测试数据
         TableInfoEditRequest tableInfoEditRequest = new TableInfoEditRequest();
-        long UpdateId = 1819578907435847681L;
-        String updateName = "updated 测试表";
-        tableInfoEditRequest.setName(updateName);
-        tableInfoEditRequest.setId(UpdateId);
-
-        // 创建cookie并添加到请求中
-        MockCookie cookie = new MockCookie(StpUtil.getTokenName(), token);
+        tableInfoEditRequest.setName("edit测试表");
+        tableInfoEditRequest.setId(1820740567387107329L);
 
         // 发送请求并验证结果
         mockMvc.perform(MockMvcRequestBuilders.post("/tableInfo/edit")
-                        .cookie(cookie) // 将token添加到cookie中
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(JSONUtil.toJsonStr(tableInfoEditRequest))) // 将整个对象序列化为JSON字符串并作为请求体发送
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data").value(true));
+    }
+
+    /**
+     * 更新表格信息测试
+     */
+    @Test
+    void UpdateTable() throws Exception {
+        // 准备测试数据
+        TableInfoUpdateRequest tableInfoUpdateRequest = new TableInfoUpdateRequest();
+        tableInfoUpdateRequest.setName("update测试表");
+        tableInfoUpdateRequest.setId(1820740567387107328L);
+
+        // 发送请求并验证结果
+        mockMvc.perform(MockMvcRequestBuilders.post("/tableInfo/update")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JSONUtil.toJsonStr(tableInfoUpdateRequest))) // 将整个对象序列化为JSON字符串并作为请求体发送
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(0))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data").value(true));
@@ -144,13 +133,27 @@ class TableInfoServiceTest extends TestBase {
      */
     @Test
     void getTableById() throws Exception {
-        long id = 1819417908070068225L;
+        long id = 1820740567387107329L;
         mockMvc.perform(MockMvcRequestBuilders.get("/tableInfo/get/vo")
                         .param("id", String.valueOf(id)))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(0))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data").exists());
-
     }
 
+    @Test
+    void doReviewTest() throws Exception {
+        // 准备测试数据
+        ReviewRequest reviewRequest = new ReviewRequest();
+        reviewRequest.setId(1820740567387107329L);
+        reviewRequest.setReviewStatus(2);
+
+        // 发送请求并验证结果
+        mockMvc.perform(MockMvcRequestBuilders.post("/tableInfo/review")
+                        .contentType(MediaType.APPLICATION_JSON) // 设置Content-Type为application/json
+                        .content(JSONUtil.toJsonStr(reviewRequest)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data").value(true));
+    }
 }
--- src/test/resources/db/h2/data.sql
+++ src/test/resources/db/h2/data.sql
@@ -1,4 +1,22 @@
 -- -- 清空所有的数据
-INSERT INTO user_info (id, userAccount, userPassword, unionId, mpOpenId, userName, userAvatar, userProfile, userRole, createTime, updateTime, isDelete) VALUES (1816001696590692353, 'cong', '3a20abef00881f3a9549f50d5d507f99', null, null, '葱皮', 'https://p4.itc.cn/q_70/images03/20220316/2b2fbf81affb4dc1a04e5a6d8ca26c99.jpeg', null, 'admin', '2024-07-24 06:45:18', '2024-08-02 14:10:43', 0);
-INSERT INTO user_info (id, userAccount, userPassword, unionId, mpOpenId, userName, userAvatar, userProfile, userRole, createTime, updateTime, isDelete) VALUES (1816992328458883074, 'congg', 'a05154021e1d346599f3042b879d3c04', null, null, null, null, null, 'user', '2024-07-27 00:21:43', '2024-07-27 00:21:43', 0);
-INSERT INTO user_info (id, userAccount, userPassword, unionId, mpOpenId, userName, userAvatar, userProfile, userRole, createTime, updateTime, isDelete) VALUES (1818965526718836738, 'shing', '3a20abef00881f3a9549f50d5d507f99', null, null, 'shing', 'http://124.70.210.130:9000/lbc/sp.jpg', null, 'admin', '2024-08-01 11:02:30', '2024-08-01 11:02:49', 0);
+INSERT INTO user_info (id, userAccount, userPassword, unionId, mpOpenId, userName, userAvatar, userProfile, userRole,
+                       createTime, updateTime, isDelete)
+VALUES (1816001696590692353, 'cong', '3a20abef00881f3a9549f50d5d507f99', null, null, '葱皮',
+        'https://p4.itc.cn/q_70/images03/20220316/2b2fbf81affb4dc1a04e5a6d8ca26c99.jpeg', null, 'admin',
+        '2024-07-24 06:45:18', '2024-08-02 14:10:43', 0);
+INSERT INTO user_info (id, userAccount, userPassword, unionId, mpOpenId, userName, userAvatar, userProfile, userRole,
+                       createTime, updateTime, isDelete)
+VALUES (1816992328458883074, 'congg', 'a05154021e1d346599f3042b879d3c04', null, null, null, null, null, 'user',
+        '2024-07-27 00:21:43', '2024-07-27 00:21:43', 0);
+INSERT INTO user_info (id, userAccount, userPassword, unionId, mpOpenId, userName, userAvatar, userProfile, userRole,
+                       createTime, updateTime, isDelete)
+VALUES (1818965526718836738, 'shing', '3a20abef00881f3a9549f50d5d507f99', null, null, 'shing',
+        'http://124.70.210.130:9000/lbc/sp.jpg', null, 'admin', '2024-08-01 11:02:30', '2024-08-01 11:02:49', 0);
+
+
+INSERT INTO table_info(id, name, content, reviewStatus, reviewMessage, userId, createTime, updateTime, isDelete)
+values (1820740567387107329, '测试测试表', '[{"id":"12345"},{"name":"shing"},{"age":"18"}]', '0', '',
+        '1816001696590692353', '2024-08-06 08:35:53', '2024-08-06 08:35:53', 0);
+INSERT INTO  table_info(id, name, content, reviewStatus, reviewMessage, userId, createTime, updateTime, isDelete)
+values (1820740567387107328, '测试测试表', '[{"id":"12345"},{"name":"shing"},{"age":"18"}]', '0', '',
+        '1816001696590692353', '2024-08-06 08:35:53', '2024-08-06 08:35:53', 0)
