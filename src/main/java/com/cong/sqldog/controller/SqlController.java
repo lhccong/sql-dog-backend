c972aafcc46dfb5481e359245f07dfd64b3a5f60
xiangxiang <1964362087@qq.com>
2024-08-11 01:59:05 +0800

    feat：补充 plantUML 实体代码生成逻辑

--- src/main/java/com/cong/sqldog/controller/SqlController.java
+++ src/main/java/com/cong/sqldog/controller/SqlController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 /**
  * SQL 控制器
  *
