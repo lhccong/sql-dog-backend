bbec6bcff72ba5d865c5069c6eea62ec3c98d066
lhccong <liuhuaicong@zhut.cn>
2024-08-15 10:10:30 +0800

    fix：修复注释抽取 bug、优化代码结构

--- src/main/java/com/cong/sqldog/core/sqlgenerate/builder/TableSchemaBuilder.java
+++ src/main/java/com/cong/sqldog/core/sqlgenerate/builder/TableSchemaBuilder.java
@@ -16,6 +16,8 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * SQL 转表概要构建器
@@ -70,11 +72,8 @@ public class TableSchemaBuilder {
             // 获取并处理表注释
             String tableComment = null;
             if (sqlCreateTableStatement.getComment() != null) {
-                tableComment = sqlCreateTableStatement.getComment().toString();
                 // 去除注释的引号
-                if (tableComment.length() > 2) {
-                    tableComment = tableComment.substring(1, tableComment.length() - 1);
-                }
+                tableComment = getExtractComment(sqlCreateTableStatement.getComment().toString());
             }
             tableSchema.setTableComment(tableComment);
 
@@ -85,59 +84,10 @@ public class TableSchemaBuilder {
             for (SQLTableElement sqlTableElement : sqlCreateTableStatement.getTableElementList()) {
                 // 处理主键约束
                 if (sqlTableElement instanceof SQLPrimaryKey sqlPrimaryKey) {
-                    // 获取主键字段名
-                    String primaryFieldName = sqlDialect.parseFieldName(sqlPrimaryKey.getColumns().get(0).toString());
-
-                    // 将主键字段设置为主键
-                    fieldList.forEach(field -> {
-                        if (field.getFieldName().equals(primaryFieldName)) {
-                            field.setPrimaryKey(true);
-                        }
-                    });
-
+                    setPrimaryKey(sqlPrimaryKey, fieldList);
                     // 处理列相关
                 } else if (sqlTableElement instanceof SQLColumnDefinition columnDefinition) {
-                    // 创建并初始化 TableSchema.Field 对象
-                    TableSchema.Field field = new TableSchema.Field();
-                    field.setFieldName(sqlDialect.parseFieldName(columnDefinition.getNameAsString()));
-                    field.setFieldType(columnDefinition.getDataType().toString());
-
-                    // 获取列的默认值
-                    String defaultValue = null;
-                    if (columnDefinition.getDefaultExpr() != null) {
-                        defaultValue = columnDefinition.getDefaultExpr().toString();
-                    }
-                    field.setDefaultValue(defaultValue);
-
-                    // 设置列的非空约束
-                    field.setNotNull(columnDefinition.containsNotNullConstaint());
-
-                    // 获取并处理列注释
-                    String comment = null;
-                    if (columnDefinition.getComment() != null) {
-                        comment = columnDefinition.getComment().toString();
-                        // 去除注释的引号
-                        if (comment.length() > 2) {
-                            comment = comment.substring(1, comment.length() - 1);
-                        }
-                    }
-                    field.setComment(comment);
-
-                    // 设置列的主键标志、是否自增、更新值等属性
-                    field.setPrimaryKey(columnDefinition.isPrimaryKey());
-                    field.setAutoIncrement(columnDefinition.isAutoIncrement());
-
-                    String onUpdate = null;
-                    if (columnDefinition.getOnUpdate() != null) {
-                        onUpdate = columnDefinition.getOnUpdate().toString();
-                    }
-                    field.setOnUpdate(onUpdate);
-
-                    // 设置字段的 Mock 类型（默认为 NONE）
-                    field.setMockType(MockTypeEnum.NONE.getValue());
-
-                    // 将字段添加到字段列表中
-                    fieldList.add(field);
+                    setColumnKey(columnDefinition, fieldList);
                 }
             }
 
@@ -149,6 +99,80 @@ public class TableSchemaBuilder {
             // 记录异常并抛出业务异常
             log.error("SQL 解析错误", e);
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "请确认 SQL 语句正确");
+        }
+    }
+
+    private static void setColumnKey(SQLColumnDefinition columnDefinition, List<TableSchema.Field> fieldList) {
+        // 创建并初始化 TableSchema.Field 对象
+        TableSchema.Field field = new TableSchema.Field();
+        field.setFieldName(sqlDialect.parseFieldName(columnDefinition.getNameAsString()));
+        field.setFieldType(columnDefinition.getDataType().toString());
+
+        // 获取列的默认值
+        String defaultValue = null;
+        if (columnDefinition.getDefaultExpr() != null) {
+            defaultValue = columnDefinition.getDefaultExpr().toString();
+        }
+        field.setDefaultValue(defaultValue);
+
+        // 设置列的非空约束
+        field.setNotNull(columnDefinition.containsNotNullConstaint());
+
+        // 获取并处理列注释
+        String comment = null;
+        if (columnDefinition.getComment() != null) {
+            comment = columnDefinition.getComment().toString();
+            // 去除注释的引号
+            if (comment.length() > 2) {
+                comment = comment.substring(1, comment.length() - 1);
+            }
+        }
+        field.setComment(comment);
+
+        // 设置列的主键标志、是否自增、更新值等属性
+        field.setPrimaryKey(columnDefinition.isPrimaryKey());
+        field.setAutoIncrement(columnDefinition.isAutoIncrement());
+
+        String onUpdate = null;
+        if (columnDefinition.getOnUpdate() != null) {
+            onUpdate = columnDefinition.getOnUpdate().toString();
+        }
+        field.setOnUpdate(onUpdate);
+
+        // 设置字段的 Mock 类型（默认为 NONE）
+        field.setMockType(MockTypeEnum.NONE.getValue());
+
+        // 将字段添加到字段列表中
+        fieldList.add(field);
+    }
+
+    private static void setPrimaryKey(SQLPrimaryKey sqlPrimaryKey, List<TableSchema.Field> fieldList) {
+        // 获取主键字段名
+        String primaryFieldName = sqlDialect.parseFieldName(sqlPrimaryKey.getColumns().get(0).toString());
+
+        // 将主键字段设置为主键
+        fieldList.forEach(field -> {
+            if (field.getFieldName().equals(primaryFieldName)) {
+                field.setPrimaryKey(true);
+            }
+        });
+    }
+
+    private static String getExtractComment(String comment) {
+        // 定义正则表达式
+        String regex = "'([^']*)'";
+
+        // 编译正则表达式
+        Pattern pattern = Pattern.compile(regex);
+
+        // 匹配字符串
+        Matcher matcher = pattern.matcher(comment);
+
+        // 查找并打印结果
+        if (matcher.find()) {
+            return matcher.group(1);  // 输出：用户
+        } else {
+            return "";
         }
     }
 
